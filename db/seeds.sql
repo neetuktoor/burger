27938a773e45957eb4862bd9ccf128aa39dec2a2
Neetu Toor <neetutoor@Neetus-Air.attlocal.net>
2017-08-08 23:52:16 -0500

    styling changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,7 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesebuger w/ bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesebuger w/ extra cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesebuger w/ guac', false);
 
