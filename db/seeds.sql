7279574aa7f3de49aaa28aecff57f59ea8b8d489
Neetu Toor <neetutoor@Neetus-Air.attlocal.net>
2017-08-08 22:06:08 -0500

    everything works

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesebuger w/ bacon', false);
+
